9f1b0e68a8809703237b745e2cf0331208e2eab4
keidaimitsuyama <ro.keidai@gmail.com>
2019-02-01 22:54:07 +0000

    減少

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ rl.on('line', (line) => {
         let value = prefectureDataMap.get(prefecture);
         if (!value) {
             value = {
-                popu10: 0,
+                 popu10: 0,
                 popu15: 0,
                 change: null
             };
@@ -35,8 +35,8 @@ rl.on('close', () => {
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
         return pair2[1].change - pair1[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+        return (i + 1) + '位' + key + ':' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
     });
     console.log(rankingStrings);
 });
